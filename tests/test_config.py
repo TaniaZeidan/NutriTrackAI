25fcb5740a05f035188a51c750361e7db2830f77
Tania Zeidan <tania.zeidan@lau.edu>
2025-12-07 13:45:49 +0200

    delete tests

--- tests/test_config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-def test_get_google_api_key():
-    load_dotenv()
-    key = os.getenv("GOOGLE_API_KEY")
-    testVal = os.getenv("TEST_ENV")
-    assert testVal == 'abc'
-    assert key == ''
